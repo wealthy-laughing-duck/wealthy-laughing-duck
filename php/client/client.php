a40e9bd837938af1e597b0de1b718cc7ab6fad5a
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-16 13:47:47 +0100

    fixed port number

--- php/client/client.php
+++ php/client/client.php
@@ -5,9 +5,9 @@ require_once('Thrift.php');
 
 use \SymfonyWorld\Thrift;
 
-$client = new Thrift("localhost", 9091);
-$add = $client->getClient()->add(10, 20);
-$sub = $client->getClient()->sub(421, 65);
+$client = new Thrift("localhost", 9090);
+$add = $client->getClient()->add(10, 67);
+$sub = $client->getClient()->sub(421, 194);
 echo "Addition result is $add and subtraction result is $sub\n";
 // = $client->executeCommand("add", array('n1' => 10, 'n2' => 20));
 
