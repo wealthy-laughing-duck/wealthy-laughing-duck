05eaea60918f5496b787057dd185b7b350ab5eef
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-23 18:26:17 +0100

    moved thrift files into src/main

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-thrift -r --gen java -out src/main/java service.thrift
-thrift -r --gen php:oop,namespace,autoload -out src/main/php/packages service.thrift
+thrift -r --gen java -out src/main/java src/main/thrift/service.thrift
+thrift -r --gen php:oop,namespace,autoload -out src/main/php/packages src/main/thrift/service.thrift
 
