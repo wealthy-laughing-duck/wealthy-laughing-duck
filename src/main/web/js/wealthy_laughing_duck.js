6f4da9182fd9b66d375f7171222c5c9ffd48bd36
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-04-07 00:03:50 +0200

    moved WealthyLaughingDuckControl into separate file

--- src/main/web/js/wealthy_laughing_duck.js
+++ src/main/web/js/wealthy_laughing_duck.js
@@ -278,47 +278,6 @@ var TemplateManager = {
     }
 }
 
-var WealthyLaughingDuckControl = {
-    currency: 'zł',
-    getCurrency: function() {
-        return this.currency;
-    },
-    parseListIntoTree: function(categories) {
-        if (categories == null)
-            return null;
-
-        var itemsByID = [];
-
-        categories.forEach(function(item) {
-            itemsByID[item.id] = {
-                data: {title: item.name},
-                attr: {id: item.id},
-                parentID: item.parent_id,
-                state: item.state
-            };
-        });
-
-        itemsByID.forEach(function(item) {
-            if(item.parentID !== null) {
-                if (typeof itemsByID[item.parentID].children === "undefined") {
-                    itemsByID[item.parentID].children = [];
-                }
-                itemsByID[item.parentID].children.push(item);
-            }
-        });
-
-        var roots = itemsByID.filter(function(item) {
-            return item.parentID === null;
-        });
-
-        itemsByID.forEach(function(item) {
-            delete item.parentID;
-        });
-
-        return roots;
-    }
-};
-
 $(document).ready( function() {
     TemplateEngine.fetchConcatenatedTemplates();
     TemplateManager.initAllTemplates();
