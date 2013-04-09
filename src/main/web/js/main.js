50d6d2c3356c8aacc6b9238b48e7249b252c47da
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-04-09 22:28:22 +0200

    money format validation improved

--- src/main/web/js/main.js
+++ src/main/web/js/main.js
@@ -1,6 +1,6 @@
 jQuery.validator.addMethod("money", function(value, element) {
-    return this.optional(element) || /^(\d{1,3})(\.\d{1,2})?$/.test(value);
-}, "Must be proper currency format 0.99");
+    return this.optional(element) || /^(\d{1,6})(\.\d{1,2})?$/.test(value);
+}, "Must be proper currency format 'dddddd.dd' (d - digit)");
 
 $(document).ready( function() {
     TemplateEngine.fetchConcatenatedTemplates();
