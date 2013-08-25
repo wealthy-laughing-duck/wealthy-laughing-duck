6e7e29369842e9a39af7307fda7a397d84fefd56
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-08-25 17:30:04 +0200

    removed local sql and thrift (using duck-commons)

--- src/main/java/com/blogspot/symfonyworld/wealthylaughingduck/model/User.java
+++ src/main/java/com/blogspot/symfonyworld/wealthylaughingduck/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "sf_guard_user")
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/sql/access.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'duck'@'localhost' IDENTIFIED BY 'pILqd9a5ImNr9RG4';
-GRANT ALL PRIVILEGES ON `duck_finance`.* TO 'duck'@'localhost'
-WITH GRANT OPTION;
